31166ca252e3462d51388dd07e80815df1a261cc
braicandrei <andrei.braic96@gmail.com>
2025-04-03 14:05:20 +0200

    some tests

--- tests/logs/FirsrLogs/analize.m
+++ tests/logs/FirsrLogs/analize.m
@@ -1,8 +1,23 @@
 clear; close all;
 
-load data
+fileName = '890632.csv';
+Fs = 225;
+data = importfile(fileName);
 
-figure;
-plot(datalog1.VarName9);
-hold on
-plot(datalog2.VarName9);
+acc = table2array(data(:,1:3));
+gyro = table2array(data(:,4:6));
+mag = table2array(data(:,7:9));
+
+accSI = acc*9.81;
+gyroSI = gyro*(pi/180);
+magSI = mag*100;
+
+fuse = complementaryFilter('SampleRate',Fs);
+q = fuse(accSI,gyroSI,magSI);
+
+plot(eulerd( q, 'ZYX', 'frame'));
+grid on
+grid minor
+title('Orientation Estimate');
+legend('Z-rotation', 'Y-rotation', 'X-rotation');
+ylabel('Degrees');
